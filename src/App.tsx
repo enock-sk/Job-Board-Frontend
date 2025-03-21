2585f95fe2866be29a8780e3e42f1a57f13bfb8c
kiplaa <ekiplangat64@gmail.com>
2025-03-21 17:59:26 +0300

    adding footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,51 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import JobList from "./components/JobList";
-import Register from "./components/Register";
-import Login from "./components/Login";
+import JobList from "./JobList";
+import Register from "./Register";
+import Login from "./Login";
+import Footer from "./Footer";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Navbar bg="primary" variant="dark" expand="lg" sticky="top">
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            JobSpark
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/jobs">
-                Jobs
-              </Nav.Link>
-              <Nav.Link as={Link} to="/register">
-                Register
-              </Nav.Link>
-              <Nav.Link as={Link} to="/login">
-                Login
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+      <div className="d-flex flex-column min-vh-100">
+        {/* Top Navbar */}
+        <Navbar bg="primary" variant="dark" expand="lg" sticky="top">
+          <Container>
+            <Navbar.Brand as={Link} to="/">
+              JobSpark
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link as={Link} to="/jobs">
+                  Jobs
+                </Nav.Link>
+                <Nav.Link as={Link} to="/register">
+                  Register
+                </Nav.Link>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        {/* Main Content */}
+        <Container className="mt-5 flex-grow-1">
+          <Routes>
+            <Route path="/" element={<JobList />} />
+            <Route path="/jobs" element={<JobList />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
         </Container>
-      </Navbar>
-      <Container className="mt-5">
-        <Routes>
-          <Route path="/" element={<JobList />} />
-          <Route path="/jobs" element={<JobList />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Container>
+
+        {/* Footer */}
+        <Footer />
+      </div>
     </Router>
   );
 };
